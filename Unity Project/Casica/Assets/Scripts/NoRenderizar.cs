f9575ffee7384e794e602e0491a114b959acb9b5
MrJake7X <alexreverter7@gmail.com>
2018-10-30 17:07:41 +0100

    Dejar de renderizar dependiendo la camara

---
+++ Unity Project/Casica/Assets/Scripts/NoRenderizar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRenderizar : MonoBehaviour {
+
+    private GameManager manager;
+
+    Renderer[] rend;
+
+    private bool done;
+
+    void Start ()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        rend = GetComponentsInChildren<Renderer>();
+    }
+
+    void Update ()
+    {
+		if(!manager.onHab)
+        {
+            foreach (Renderer r in rend)
+            {
+                r.enabled = false;
+            }
+            /*
+            for (int i = 0; 1 < rend.Length; i++)
+            {
+                rend[i].enabled = false;
+                Debug.Log(rend[i].transform.name);
+            }
+            */
+            //done = true;
+        }
+	}
+}
