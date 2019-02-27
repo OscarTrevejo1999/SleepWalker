caf74709d199211534cc03b2c0009d72942a0f13
Aitor <Aitor@gmail.com>
2019-02-27 16:26:28 +0100

    CosicasOscar

---
+++ Unity Project/Casica/Assets/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    private PlayerController player;
+
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+	
+	void Update ()
+    {
+        //Player movement
+        Vector2 inputAxis = Vector2.zero;
+        inputAxis.x = Input.GetAxis("Horizontal");
+        inputAxis.y = Input.GetAxis("Vertical");
+
+        player.SetAxis(inputAxis);
+    }
+}
---
+++ Unity Project/Casica/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector2 axis;
+
+    private CharacterController controller;
+
+    public float speed;
+
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+
+    
+	
+	void Update ()
+    {
+
+        Vector3 transformDirection = axis.x * transform.right + axis.y * transform.forward;
+
+        moveDirection.x = transformDirection.x * speed;
+        moveDirection.z = transformDirection.z * speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+
+    public void SetAxis(Vector2 inputAxis)
+    {
+        axis = inputAxis;
+    }
+
+}
